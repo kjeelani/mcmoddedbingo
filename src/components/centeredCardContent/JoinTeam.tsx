12aefcd04c1f0fdd247c772089fa3bf2b26af9fe
GitYACC <shastrysamarth@gmail.com>
2023-06-07 20:25:48 -0700

    join team page update

--- src/components/centeredCardContent/JoinTeam.tsx
+++ src/components/centeredCardContent/JoinTeam.tsx
@@ -3,19 +3,49 @@ import { useFetch } from "../lib/AxiosHooks"
 import { 
     Stack,
     Text,
+    useDisclosure,
+    Link
 } from '@chakra-ui/react'
 import { GoogleSignOut, GoogleSignOutProps} from "./GoogleSignOutButton"
 import { TeamData, Teams } from "../lib/APIData";
 import { TeamRow, TeamRowProps } from "../utility/TeamRow";
+import { TeamModal, TeamModalProps } from '@/components/utility/TeamModal';
 
 export interface JoinTeamProps {
 	children: React.ReactNode;
 }
 
+interface TeamRowModalProps {
+    team: TeamData
+}
+
+function TeamRowModal(props: TeamRowModalProps) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <>
+            <Link 
+                onClick={onOpen}
+                _hover={{
+                    textDecoration: "none"
+                }}
+            >
+                <TeamRow spacing={3} key={props.team.teamID} team={props.team} />
+            </Link>
+            <TeamModal
+                teamName={props.team.teamName}
+                team={props.team.players}
+                isOpen={isOpen}
+                onClose={onClose}
+            ></TeamModal>
+        </>
+    )
+}
+
 function listTeams(data: Teams) {
     return (
         data.teams.map((team: TeamData) => {
-            return <TeamRow spacing={3} key={team.teamID} team={team} />
+            return <TeamRowModal team={team}></TeamRowModal>
         })
     );
 }
@@ -25,6 +55,8 @@ export function JoinTeam(jtprops: JoinTeamProps) {
         url: `api/teams`,
         manual: true
     });
+
+    
     
     useEffect(() => {
         if (!data) {
